fe6c5e08a6c71987eefb5fa9be123219055b036a
evgeniy <pavlinb4@gmail.com>
2022-09-16 16:44:38 +0300

    очень интересно, программа говорит что добавила xmp  данные  в ORF а PhotoMechanic их не видит

---
+++ try_PyExifTool.py
@@ -0,0 +1,29 @@
+import exiftool
+
+file = "/Users/evgeniy/Pictures/2022/20220630_Юридический форум/20220630EPAV0184.ORF"
+
+
+def read_xmp():
+    with exiftool.ExifToolHelper() as et:
+        metadata = et.get_metadata(file)
+
+    # for d in metadata[0].keys():
+    #     if d.startswith("XMP"):
+    #         print(d, metadata[0][d])
+    return metadata[0]["XMP:Title"], metadata[0]['XMP:Label'], metadata[0]['XMP:Creator']
+
+
+def write_data_to_photo():
+    with exiftool.ExifToolHelper() as et:
+        title = "title of the image"
+        et.set_tags(
+            [file],
+            tags={"XMP:Title": title,
+                  'XMP:Label': "Yellow",
+                  'XMP:Creator': 'Eugene Pavlenko'},
+            params=["-P", "-overwrite_original"]
+        )
+
+
+# write_data_to_photo()
+print(read_xmp())
