f1ae79700ce8f7bca0b5e05bda065aff81f89e8c
evgeniy <pavlinb4@gmail.com>
2022-09-16 17:11:33 +0300

    c NEF та же беда, не видно результатов в Photomechanic

--- try_PyExifTool.py
+++ try_PyExifTool.py
@@ -1,6 +1,7 @@
 import exiftool
 
 file = "/Users/evgeniy/Pictures/2022/20220630_Юридический форум/20220630EPAV0184.ORF"
+file = '/Users/evgeniy/Pictures/2022/20220914_Северная звезда/20220914PEV_6785.NEF'
 
 
 def read_xmp():
@@ -8,22 +9,26 @@ def read_xmp():
         metadata = et.get_metadata(file)
 
     # for d in metadata[0].keys():
-    #     if d.startswith("XMP"):
-    #         print(d, metadata[0][d])
+        # print(d, metadata[0][d])
+        # if d.startswith("XMP"):
+        #     print(d, metadata[0][d])
+        # if d.startswith("IPTC"):
+        #     print(d, metadata[0][d])
     return metadata[0]["XMP:Title"], metadata[0]['XMP:Label'], metadata[0]['XMP:Creator']
 
 
 def write_data_to_photo():
     with exiftool.ExifToolHelper() as et:
-        title = "title of the image"
+        title = "exiftool NEF"
         et.set_tags(
             [file],
             tags={"XMP:Title": title,
+                  "IPTC:ObjectName": title,
                   'XMP:Label': "Yellow",
                   'XMP:Creator': 'Eugene Pavlenko'},
             params=["-P", "-overwrite_original"]
         )
 
 
-# write_data_to_photo()
+write_data_to_photo()
 print(read_xmp())
