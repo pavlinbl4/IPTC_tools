af6f1dbaaddc96c5a62ad303f81078b940da5101
evgeniy_MacBook <pavlinbl4@gmail.com>
2023-07-07 23:18:13 +0300

    work with image metadata with exiftool

--- try_PyExifTool.py
+++ try_PyExifTool.py
@@ -1,9 +1,12 @@
+# pip install PyExifTool
 import exiftool
 
 # jpg_file = '/Users/evgeniy/Pictures/python_test_images/shutterstock_235800871.jpg'
-jpg_file = '/Users/evgeniy/Pictures/python_test_images/20230602_PAV5801.JPG'
+# jpg_file = '/Users/evgeniy/Pictures/python_test_images/20230602_PAV5801.JPG'
 # dng_file = '/Users/evgeniy/Pictures/python_test_images/20230602_PAV5801.DNG'
-# orf_file = '/Users/evgeniy/Pictures/python_test_images/20230403EPAV9047.ORF'
+orf_file = '/Users/evgeniy/Pictures/Pyton_tes_images/20230620EPAV8084.ORF'
+
+
 # jpg_file = '/Users/evgeniy/Pictures/python_test_images/20211201PEV_2795-Edit.JPG'
 # jpg_file_edit = '/Users/evgeniy/Pictures/python_test_images/20230403EPAV9047_xnView.jpg'
 
@@ -15,22 +18,27 @@ def read_all_metadata_by_type(file, metadata_type):
         if d.startswith(metadata_type):
             print(d, '-->', metadata[d])
 
+
 def read_main_xmp_data(file):
     with exiftool.ExifToolHelper() as et:
         metadata = et.get_metadata(file)[0]  # extract dict from list
-    main_tags = ['XMP:Headline','XMP:Description', 'XMP:Rating', 'XMP:ColorClass', 'XMP:Tagged', 'XMP:Subject', 'XMP:Urgency']
-    # main_tags = ['XMP:RawFileName','XMP:OriginalDocumentID','XMP:PreservedFileName','XMP:DerivedFromOriginalDocumentID']
+    main_tags = ['XMP:Headline', 'XMP:Description', 'XMP:Rating', 'XMP:ColorClass', 'XMP:Tagged', 'XMP:Subject',
+                 'XMP:Urgency']
     readed_tags = {}
     for tag in main_tags:
         readed_tags[tag.split(':')[1]] = metadata.get(tag, 'NO INFORMATION')
     return readed_tags
+
 
 def read_keywords(file):
     with exiftool.ExifToolHelper() as et:
         metadata = et.get_metadata(file)[0]  # extract dict from list
     return metadata.get('XMP:Subject', [])
 
-
+def check_tag(file):
+    with exiftool.ExifToolHelper() as et:
+        metadata = et.get_metadata(file)[0]  # extract dict from list
+    return metadata.get('XMP:Tagged')
 
 
 def write_data_to_photo(file):
@@ -50,19 +58,20 @@ def write_data_to_photo(file):
                   'XMP:Creator': 'Eugene Pavlenko',
                   'XMP:Subject': "Python",
 
-                 },  #  'XMP:Subject': keywords.append("Python")
+                  },  # 'XMP:Subject': keywords.append("Python")
             params=["-P", "-overwrite_original"]
         )
-
 
 
 if __name__ == '__main__':
     # write_data_to_photo()
     # read_all_metadata_by_type(jpg_file, "IPTC")
     # read_all_metadata_by_type(jpg_file, "XMP")
-    # print(read_main_xmp_data(jpg_file))
+    # print(read_main_xmp_data(orf_file))
+    print(check_tag(orf_file))
+
     # write_data_to_photo(jpg_file_edit)
     # print(read_main_xmp_data(jpg_file_edit))
     # print(read_keywords(jpg_file))
     # print(type(read_keywords(jpg_file)))
-    write_data_to_photo(jpg_file)
+    # write_data_to_photo(jpg_file)
